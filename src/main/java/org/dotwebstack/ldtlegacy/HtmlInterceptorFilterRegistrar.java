3ae4a024e1a6dc78a7ac698f2230f4a3a2752e12
Philipp Juergensen <Philipp.Juergensen@ovsoftware.com>
2018-03-13 09:38:44 +0100

    Change request handler to endpoint

--- src/main/java/org/dotwebstack/ldtlegacy/HtmlInterceptorFilterRegistrar.java
+++ src/main/java/org/dotwebstack/ldtlegacy/HtmlInterceptorFilterRegistrar.java
@@ -3,7 +3,7 @@ package org.dotwebstack.ldtlegacy;
 import javax.ws.rs.container.DynamicFeature;
 import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.FeatureContext;
-import org.dotwebstack.framework.frontend.ld.handlers.RepresentationRequestHandler;
+import org.dotwebstack.framework.frontend.ld.handlers.EndPointRequestHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,7 +13,7 @@ public class HtmlInterceptorFilterRegistrar implements DynamicFeature {
 
   @Override
   public void configure(ResourceInfo resourceInfo, FeatureContext context) {
-    if (RepresentationRequestHandler.class.equals(resourceInfo.getResourceClass())
+    if (EndPointRequestHandler.class.equals(resourceInfo.getResourceClass())
         && resourceInfo.getResourceMethod().getName().equals("apply")) {
       context.register(HtmlWriterInterceptor.class);
       context.register(LegacyResponseFilter.class);
